45b88e2ff77a6734d5b442ded4d0c0cdfb21e7c4
İbrahim Erdoğan <kararlibiri@gmail.com>
2025-06-26 21:03:17 +0300

    add files to github repo

---
+++ src/client/index.js
@@ -0,0 +1,96 @@
+// Copyright (c) 2021 Zero Density Inc.
+//
+// This file is part of realityhub-module-example.
+//
+// realityhub-module-example is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2, as published by 
+// the Free Software Foundation.
+//
+// realityhub-module-example is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with realityhub-module-example. If not, see <https://www.gnu.org/licenses/>.
+
+class RestApiModuleClient {
+  
+  async start() {
+    this.containerElement = document.createElement('div');
+
+    const initModule = (params) => {
+      const { brokerClient } = params;
+
+      this.api = brokerClient.api.zero_density.realityhub_restapi;  
+
+      return this.containerElement;
+    }
+    
+    const destroyModule = () => {
+      if(this.containerElement) {
+        this.containerElement.remove();
+      }
+
+    }
+
+    await window.registerRealityHubModule({
+      name: 'zero_density.realityhub_restapi_client',
+      label: 'RealityHub REST API',
+      init: (registrationResult) => initModule(registrationResult),
+      destroy: () => destroyModule(),
+    });
+
+    // Download Module Example's HTML file and set it to our container element
+    const response = await fetch('/modules/zero_density.realityhub_restapi/index.html');
+    this.containerElement.innerHTML = await response.text();
+
+    // Swagger UI dynamic load functions
+    function loadScript(url) {
+      return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+      });
+    }
+
+    async function loadSwaggerUI() {
+      // Add CSS dynamically too
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = '/modules/zero_density.realityhub_restapi/swagger-ui/swagger-ui.css';
+      document.head.appendChild(link);
+
+      const darkCss = document.createElement('link');
+      darkCss.rel = 'stylesheet';
+      darkCss.href = '/modules/zero_density.realityhub_restapi/SwaggerDark.user.css';
+      document.head.appendChild(darkCss);
+
+      // Load JS files sequentially
+      await loadScript('/modules/zero_density.realityhub_restapi/swagger-ui/swagger-ui-bundle.js');
+      await loadScript('/modules/zero_density.realityhub_restapi/swagger-ui/swagger-ui-standalone-preset.js');
+
+      // Start Swagger UI
+      const ui = window.SwaggerUIBundle({
+        url: '/modules/zero_density.realityhub_restapi/realityhub-api.yaml',
+        dom_id: '#swagger-ui',
+        presets: [
+          window.SwaggerUIBundle.presets.apis,
+          window.SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout"
+      });
+      window.ui = ui;
+    }
+
+    // Launch Swagger UI after HTML is loaded
+    loadSwaggerUI();
+  }
+
+
+}
+
+const restApiModuleClient = new RestApiModuleClient();
+restApiModuleClient.start();
---
+++ src/server/index.js
@@ -0,0 +1,100 @@
+// Copyright (c) 2021 Zero Density Inc.
+//
+// This file is part of realityhub-module-example.
+//
+// realityhub-module-example is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2, as published by 
+// the Free Software Foundation.
+//
+// realityhub-module-example is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with realityhub-module-example. If not, see <https://www.gnu.org/licenses/>.
+
+const { BrokerClient } = require("@zerodensity/realityhub-api");
+const express = require('express');
+const path = require('path');
+
+const swaggerUiDist = require('swagger-ui-dist').getAbsoluteFSPath();
+const REALITY_HUB_PORT = process.env.REALITY_HUB_PORT || 80;
+
+class RestApiModuleBackend {
+
+
+  async initBroker() {
+    this.brokerClient = await BrokerClient.initModule({
+      menuTitle: 'RealityHub REST API',
+      clientModuleName: 'zero_density.realityhub_restapi_client',
+      moduleName: 'zero_density.realityhub_restapi',
+      serverURL: 'http://127.0.0.1:5000/',
+      hub: {
+        host: '127.0.0.1',
+        port: REALITY_HUB_PORT,
+      },
+    });
+
+    this.api = this.brokerClient.api.zero_density.realityhub_restapi;
+
+    this.brokerClient.once('disconnect', () => {
+      this.brokerClient.destroy();
+      this.stopPolling();
+      this.restart();
+    });
+  }
+
+  startHTTPServer() {
+    const app = express();
+
+    app.use('/swagger-ui', express.static(swaggerUiDist));
+
+    app.use(express.static(path.join(__dirname, '../client')));
+
+    app.listen(5000, '0.0.0.0', () => {
+      console.info('REST API Documentation backend started on port 5000');
+    });
+  }
+
+  init() {
+    this.startHTTPServer();
+    this.restart();
+  }
+
+  async restart() {
+    try {
+      await this.initBroker();
+    } catch(e) {
+      console.error('Unable to initialize Broker, exiting..');
+      process.exit(1);
+    }
+
+    this.startPolling();
+  }
+
+  startPolling() {
+    if (this.pollTimer) return;
+   
+    this.api.emit('statuschange', { status: 'started' });
+  }
+
+  stopPolling() {
+    if (!this.pollTimer) return;
+
+    clearTimeout(this.pollTimer);
+    this.pollTimer = null;
+    this.api.emit('statuschange', { status: 'stopped' });
+  }
+
+  getStatus() {
+    return {
+      status: !!this.pollTimer ? 'started' : 'stopped',
+    };
+  }
+
+
+}
+
+const restApiModuleBackend = new RestApiModuleBackend();
+restApiModuleBackend.init();
